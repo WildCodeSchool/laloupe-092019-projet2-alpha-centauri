6ac2d9411f88448448ea38766978d1c9a42b5f63
dzendzen <dzen@hotmail.fr>
2019-10-23 16:42:02 +0200

    footer to component

--- projet2/src/App.js
+++ projet2/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Intro from './components/Intro';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
     <Intro/>
+    <Footer/>
     </div>
     );
   }
