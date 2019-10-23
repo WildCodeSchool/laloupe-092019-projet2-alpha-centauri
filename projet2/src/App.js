ff3f7019d0bec4dbe2e9affdadeab5e726a9054f
Patrice Leroux <lerouxp541@gmail.com>
2019-10-23 16:46:12 +0200

    menu burger navbar tab mob

--- projet2/src/App.js
+++ projet2/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Intro from './components/Intro';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div>
+    <Navbar/>
     <Intro/>
     </div>
     );
